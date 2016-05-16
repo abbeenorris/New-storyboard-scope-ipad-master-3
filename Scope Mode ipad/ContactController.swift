6f83a2f4a2df16170911aec5ff1f6d6bd717b138
Samuel Gray <sam.gray453@gmail.com>
2016-05-16 18:20:57 +0100

    mail fixing

--- Scope Mode ipad/ContactController.swift
+++ Scope Mode ipad/ContactController.swift
@@ -1,8 +1,6 @@
 import Foundation
 import UIKit
-import MessageUI
-
-class ContactController: UIViewController, MFMailComposeViewControllerDelegate {
+class ContactController: UIViewController {
     var projectTitleVar =  String()
     var clientNameVar = String()
     var videoTypeVar = String()
@@ -32,39 +30,6 @@ class ContactController: UIViewController, MFMailComposeViewControllerDelegate {
     
     
     
-    //start email (email form wont work in simulator, you'll need a device to test  )
-    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
-        let mailComposeViewController = configuredMailComposeViewController()
-        if MFMailComposeViewController.canSendMail() {
-            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
-        } else {
-            self.showSendMailErrorAlert()
-        }
-    }
-    
-    
-    func configuredMailComposeViewController() -> MFMailComposeViewController {
-        let mailComposerVC = MFMailComposeViewController()
-        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
-        
-        //touch only things in this block
-        mailComposerVC.setToRecipients(["sam.gray22@gmail.com"])
-        mailComposerVC.setSubject( "company name" + projectTitle)
-        mailComposerVC.setMessageBody("", isHTML: false)
-        //end block
-        return mailComposerVC
-    }
-    func showSendMailErrorAlert() {
-        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
-        sendMailErrorAlert.show()
-    }
-    
-    
-    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
-        controller.dismissViewControllerAnimated(true, completion: nil)
-        
-    }
-    //end email
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let projectTitle : SummaryController = segue.destinationViewController as! SummaryController
         projectTitle.projectTitleVar = projectTitleVar
--- Scope Mode ipad/SummaryController.swift
+++ Scope Mode ipad/SummaryController.swift
@@ -1,7 +1,8 @@
 import Foundation
 import UIKit
+import MessageUI
 
-class SummaryController: UIViewController {
+class SummaryController: UIViewController, MFMailComposeViewControllerDelegate {
     var projectTitleVar =  String()
     var clientNameVar = String()
     var videoTypeVar = String()
@@ -43,6 +44,43 @@ class SummaryController: UIViewController {
         deadlineText.text! = deadlineVar
         
         
+        
+        
     }
+    
+    //start email (email form wont work in simulator, you'll need a device to test  )
+    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        //touch only things in this block
+        mailComposerVC.setToRecipients(["sam.gray22@gmail.com"])
+        mailComposerVC.setSubject( "company name")
+        mailComposerVC.setMessageBody("", isHTML: false)
+        //end block
+        return mailComposerVC
+    }
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    //end email
+    
     
 }
