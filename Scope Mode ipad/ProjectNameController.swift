ca8acf1d8d3356a9a1d2c8989e89c4a5f8ace41d
kmcgowan0 <i7673473@Mac27779.local>
2016-05-18 14:00:38 +0100

    text fields follow the keyboard on first vc
    do it on the rest lol

--- Scope Mode ipad/ProjectNameController.swift
+++ Scope Mode ipad/ProjectNameController.swift
@@ -3,11 +3,46 @@ import UIKit
 class ProjectNameController: UIViewController {
     @IBOutlet weak var projectTextfield: UITextField!
     @IBOutlet weak var clientNameTextField: UITextField!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var activeField: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
 
+        
+
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        keyboardFrame = self.view.convertRect(keyboardFrame, fromView: nil)
+        
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        self.scrollView.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        
+        var contentInset:UIEdgeInsets = UIEdgeInsetsZero
+        self.scrollView.contentInset = contentInset
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let projectTitle : VideoTypeController = segue.destinationViewController as! VideoTypeController
@@ -17,6 +52,8 @@ class ProjectNameController: UIViewController {
         clientName.clientNameVar = clientNameTextField.text!
         
     }
+    
+    
 
 }
 
