55ce6d52008c4e59804a2e36b80355dcd4d22f72
kmcgowan0 <i7673473@Mac27779.local>
2016-05-18 14:22:29 +0100

    scrolling on desc view (kind of) and not at all on character

--- Scope Mode ipad/CharactersController.swift
+++ Scope Mode ipad/CharactersController.swift
@@ -21,6 +21,7 @@ class CharactersController: UIViewController {
         characterText.text! = "\(currentlVaule)"
         characterVar = characterText.text!
     }
+    @IBOutlet weak var scrollView: UIScrollView!
     
     //setting
     @IBOutlet weak var settingText: UITextField!
@@ -28,8 +29,40 @@ class CharactersController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
                    
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        keyboardFrame = self.view.convertRect(keyboardFrame, fromView: nil)
+        
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        self.scrollView.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        
+        var contentInset:UIEdgeInsets = UIEdgeInsetsZero
+        self.scrollView.contentInset = contentInset
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    
+    
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let DestViewController : PlatformController = segue.destinationViewController as! PlatformController
