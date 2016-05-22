a9622303094233b5069d477016ec6e7adeb8278b
Samuel Gray <i7672448@mac27778.local>
2016-05-22 17:20:05 +0100

    changed running time picker

--- Scope Mode ipad/RunningTimeController.swift
+++ Scope Mode ipad/RunningTimeController.swift
@@ -1,11 +1,53 @@
 import Foundation
 import UIKit
 
-class RunningTimeController: UIViewController, UIPickerViewDelegate {
+class RunningTimeController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     var projectTitleVar =  String()
     var clientNameVar = String()
     var videoTypeVar = String()
     var timeVar = String()
+    
+    var videoRunTime = 0
+    
+    
+    @IBOutlet weak var TimerView: UIPickerView!
+    
+    var timePicker: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.TimerView.delegate=self
+        self.TimerView.dataSource=self
+        
+        timePicker = ["5 minutes", "10 minutes ", "15 minutes", "20 minutes", "25 minutes", "30 minutes"]
+        
+    }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timePicker.count
+    }
+    
+    func pickerView(pickerView: UIPickerView,titleForRow row: Int, forComponent component: Int) -> String? {
+        return timePicker [row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        videoRunTime = row
+        timeVar = timePicker[row]
+        
+        print(timeVar)
+    }
+
+    
+    
+    
+    
     
     
     @IBOutlet weak var runningTimePicker: UIDatePicker!
@@ -26,10 +68,7 @@ class RunningTimeController: UIViewController, UIPickerViewDelegate {
     }
 
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let DestViewController : DeadlineController = segue.destinationViewController as! DeadlineController
