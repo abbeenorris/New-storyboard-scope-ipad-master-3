a1c0a98cebb467d82b4468ea3cce6bc13fdadd64
Samuel Gray <i7672448@mac27778.local>
2016-05-22 16:03:00 +0100

    You now have to fill out project and client name

--- Scope Mode ipad/ProjectNameController.swift
+++ Scope Mode ipad/ProjectNameController.swift
@@ -5,11 +5,15 @@ class ProjectNameController: UIViewController {
     @IBOutlet weak var clientNameTextField: UITextField!
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var nextBtn: UIButton!
     
     var projectTitleVar = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "textChanged:", name: UITextFieldTextDidChangeNotification, object: nil)
+        nextBtn.enabled = false
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
@@ -39,10 +43,22 @@ class ProjectNameController: UIViewController {
         self.scrollView.contentInset = contentInset
     }
     
+    func textChanged(sender: NSNotification) {
+        if projectTextfield.hasText() && clientNameTextField.hasText() {
+            nextBtn.enabled = true
+        }
+        else {
+            nextBtn.enabled = false
+        }
+    }
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
     }
 
+    
+    
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let projectTitle : VideoTypeController = segue.destinationViewController as! VideoTypeController
         projectTitle.projectTitleVar = projectTextfield.text!
