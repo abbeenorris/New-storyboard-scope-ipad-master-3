f090b6cf6a270e33be143ce2ff14286d892662bf
kmcgowan0 <i7673473@mac27775.local>
2016-05-23 13:43:03 +0100

    added cocoa pods

---
+++ Pods/Target Support Files/Pods-Scope Mode ipad/Pods-Scope Mode ipad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scope_Mode_ipad : NSObject
+@end
+@implementation PodsDummy_Pods_Scope_Mode_ipad
+@end
---
+++ Pods/Target Support Files/Pods-Scope Mode ipad/Pods-Scope Mode ipad-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Scope_Mode_ipadVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Scope_Mode_ipadVersionString[];
+
