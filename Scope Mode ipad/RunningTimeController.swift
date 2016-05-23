bcef5155860e0e231eb5d575e5efe7a35d5be6b4
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-05-23 14:55:10 +0100

    I DID CODE!!

--- Scope Mode ipad/RunningTimeController.swift
+++ Scope Mode ipad/RunningTimeController.swift
@@ -20,7 +20,7 @@ class RunningTimeController: UIViewController, UIPickerViewDelegate, UIPickerVie
         self.TimerView.delegate=self
         self.TimerView.dataSource=self
         
-        timePicker = ["5 minutes", "10 minutes ", "15 minutes", "20 minutes", "25 minutes", "30 minutes"]
+        timePicker = ["5 minutes", "10 minutes ", "15 minutes", "20 minutes", "25 minutes", "30 minutes", "30 minutes +"]
         
     }
     
