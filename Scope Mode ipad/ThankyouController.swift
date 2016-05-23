04ba85ea416237168a72badf9a622c4c679cd256
Abigail Norris <abbeenorris@hotmail.co.uk>
2016-05-23 15:20:43 +0100

    added thank you controller

---
+++ Scope Mode ipad/ThankyouController.swift
@@ -0,0 +1,11 @@
+//
+//  ThankyouController.swift
+//  Scope Mode ipad
+//
+//  Created by Abigail Norris on 23/05/2016.
+//  Copyright © 2016 Abigail Norris (i7696586). All rights reserved.
+//
+
+import Foundation
+
+
